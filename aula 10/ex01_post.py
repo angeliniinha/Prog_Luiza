f9e91dae0c11144a330a51daccf0e4a33fae28fd
anomalia <luiza.llocatelli@gmail.com>
2026-08-15 17:28:28 -0300

    Update ex01_post.py

--- aula 10/ex01_post.py
+++ aula 10/ex01_post.py
@@ -10,7 +10,6 @@ produtos = [
 def listar():
     return jsonify(produtos)
 
-
 @app.route("/produtos", methods=["POST"])
 def criar():
     novo = request.get_json()
@@ -18,7 +17,6 @@ def criar():
     produtos.append(novo)
 
     return jsonify(novo), 201
-
 
 if __name__ == "__main__":
     app.run(debug=True)
